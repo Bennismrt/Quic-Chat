550cc99792e8970027e3c7e6bf3946bf0b7676a0
benni setiady <bennysetiady21@gmail.com>
2022-03-10 17:33:42 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        '144': '36rem',
+        '150': '60rem',
+        '157': '53rem',
+        '123': '12.7rem',
+        '124': '13.2rem',
+        '125': '14.7rem',
+        
+      }
+    },
+  },
+  plugins: [],
+}
